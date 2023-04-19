5b18a614001273c731c0913ea48279823dfc1338
TearsOprah <qmeshokp@mail.ru>
2023-04-19 12:45:48 +0300

    feat: подключена Mongo, созданы схемы и модели пользователя и карточки

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb')
 
 app.get('/', (req, res) => {
   res.send('Привет, мир!');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
