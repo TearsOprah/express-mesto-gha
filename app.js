84128c82d3f5be92e09c0291b488c13181cfa00e
TearsOprah <qmeshokp@mail.ru>
2023-05-08 11:45:01 +0300

    fix: корневой роутер

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const auth = require('./middlewares/auth');
-const { createUser, login } = require('./controllers/users');
+const { errors } = require('celebrate');
 const NotFoundError = require('./errors/NotFound');
-const { urlRegExp } = require('./urlRegExp');
 const errorHandler = require('./errors/errorHandler');
+const rootRouter = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,32 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// роуты, которые не требуют авторизации
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi
-      .string()
-      .pattern(urlRegExp),
-  }),
-}), createUser);
-
-// авторизация
-app.use(auth);
-
-// роуты, которым нужна авторизация
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use('/', rootRouter);
 
 app.use((req, res, next) => next(new NotFoundError('Страница не найдена')));
 
